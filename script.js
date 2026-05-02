8a140b7d42d91dde78a9933334cedcd7b7a1bccf
Ethan Zuo <277018378+ethanjzuo@users.noreply.github.com>
2026-05-01 21:20:19 -0400

    slider

---
+++ script.js
@@ -0,0 +1,12 @@
+const slidy = document.getElementById("slidy");
+const output = document.getElementById("output");
+const gridy = document.querySelector(".grid");
+
+function buildGrid() {
+    const size = slidy.value;
+    output.textContent = `${size} x ${size}`;
+
+    for 
+}
+
+slidy.oninput = buildGrid;
